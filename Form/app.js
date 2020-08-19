259ed38b3e02fe6ae1618065a10c570d441a9d90
Kristen Woodward <64042155+kristenwood123@users.noreply.github.com>
2020-08-18 17:52:40 -0700

    Update app.js

--- Form/app.js
+++ Form/app.js
@@ -1,16 +1,36 @@
-const firstName = document.getElementById('firstname')
-const lastName = document.getElementById('lastname')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+const inputs = document.querySelectorAll('input')
 
-document.addEventListener('submit', (e) => {
- if (password.value.length < 8) {
-     alert('Password must be 8 characters long') 
-  e.preventDefault()
-} else {  
-    return console.log(`Name: ${firstName.value} ${lastName.value},
-    Email: ${email.value}, Password: ${password.value}`)
+const validations = {
+    firstname: /^[a-zA-Z]{0,}$/,
+    lastname: /^[a-z]{0,}$/ig,
+    password: /^[A-Za-z\d]{8,}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]){2,}(\.[a-z]{2,8})?$/ 
+}
+
+//any letters, numbers, dots or hyphens
+//any letters, numbers and/or hypens
+//any letters
+//a dot(.) then any letters s
+//(yourname) @ (domain). (extension)(.again)
+
+
+validate = (field, regex) => {
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+        console.log('it is valid');
+    } else {
+        field.className = 'invalid';
+        console.log('NOT VALID');
     }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+    // console.log(e.target.attributes.name.value)
+    validate(e.target, validations[e.target.attributes.name.value])
+    })
 })
 
 
